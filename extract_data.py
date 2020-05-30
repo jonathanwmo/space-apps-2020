6870b5b5c9e6b2b214fec632e8ad0dbe8237ef2b
Jonathan Mo <jm9hx@virginia.edu>
2020-05-29 22:48:22 -0400

    updated extract_data.py

--- extract_data.py
+++ extract_data.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 import re
 from selenium import webdriver
 import time
+from datetime import datetime, timedelta
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -86,16 +87,17 @@ def get_county_data(county: str):
     mydict = {"Total Cases": county_numbers[0], "New Cases": county_numbers[1], "Total Deaths": county_numbers[2], "New Deaths": county_numbers[3], "Active Cases": county_numbers[4], "Total Tests": county_numbers[5]}
     return mydict
 
-def get_county_climate(county: str):
+def get_county_climate(county: str, month: str):
     '''
     gets avg temp, dew point, humidity, wind speed, pressure, precipitation
     :param county: string of county to be inputted
     :return: a list of values
     '''
-    county = county.title()
-
-    driver = webdriver.Chrome(os.path.dirname(os.path.realpath(__file__)) + '/chromedriver')
-    driver.get('https://www.wunderground.com/history/monthly/us/ca/los-angeles/KLAX/date/2020-4')
+    county = county.lower().replace(" ", "-")
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    driver = webdriver.Chrome(dir_path + '/chromedriver')
+    url = 'https://www.wunderground.com/history/monthly/us/ca/' + county + '/date/2020-' + str(month)
+    driver.get(url)
     time.sleep(5)
     driver_page = driver.page_source
     page = BeautifulSoup(driver_page, 'html.parser')
@@ -104,8 +106,6 @@ def get_county_climate(county: str):
     avg_numbers_html = []
     avg_numbers = []
 
-    count = 0
-    inlines = False
     for line in page_list:
         if '</span><div class="mat-ripple mat-button-ripple" matripple=""></div><div class="mat-button-focus-overlay"></div></button><!-- --><mat-menu class=""><!-- --></mat-menu></menu-item-more></nav></lib-menu><!-- --><div _ngcontent-app-root-c137=""></div><lib-search _ngcontent-app-root-c137="" _nghost-app-root-c135="" ' in str(line):
             avg_numbers_html.append(line)
@@ -118,23 +118,55 @@ def get_county_climate(county: str):
     for i in avg_numbers:
         avg_numbers_stripped.append(i.strip())
 
-    # print(avg_numbers_html)
-    # print(avg_numbers_html)
-    print(avg_numbers_stripped)
-    # for line in page_list:
-    #     print(line)
-    #     print("\n")
+    date_aot = str(datetime.today())[0:10]
+    day = int(date_aot[-2:])
 
-get_county_climate("Los Angeles")
+    index = avg_numbers_stripped.index('5,0') + 6
+    avg_temp_list = avg_numbers_stripped[index + day::3]
+    avg_temp_list = avg_temp_list[:day]
+
+    avg_dewpoint_list = avg_numbers_stripped[index + day + (day*3)::3]
+    avg_dewpoint_list = avg_dewpoint_list[:day]
+
+    avg_humidity_list = avg_numbers_stripped[index + day + (day*6)::3]
+    avg_humidity_list = avg_humidity_list[:day]
+
+    avg_windspeed_list = avg_numbers_stripped[index + day + (day*9)::3]
+    avg_windspeed_list = avg_windspeed_list[:day]
+
+    avg_pressure_list = avg_numbers_stripped[index + day + (day*12)::3]
+    avg_pressure_list = avg_pressure_list[:day]
+
+    avg_temp_list = list(map(float, avg_temp_list))
+    avg_dewpoint_list = list(map(float, avg_dewpoint_list))
+    avg_humidity_list = list(map(float, avg_humidity_list))
+    avg_windspeed_list = list(map(float,avg_windspeed_list))
+    avg_pressure_list = list(map(float, avg_pressure_list))
+
+    dir_path = dir_path + '/csv/' + county + '.csv'
+    if not os.path.exists(dir_path):
+        with open(dir_path, 'a') as f:
+            f.write("test")
+
+    print("avg temps",avg_temp_list)
+    print("avg dew points", avg_dewpoint_list)
+    print("avg humidities", avg_humidity_list)
+    print("avg windspeeds", avg_windspeed_list)
+    print("avg pressures", avg_pressure_list)
+
+get_county_climate("San Diego", '5')
 
 
 
 # counties = ['California Total', 'Los Angeles', 'Riverside', 'San Diego', 'Orange', 'San Bernardi', 'Alameda', 'Santa Clara', 'San Francisco', 'San Mateo', 'Kern', 'Tulare', 'Santa Barbara', 'Fresno', 'Imperial', 'Contra Costa', 'Sacramento', 'Ventura', 'San Joaquin', 'Kings', 'Stanislaus', 'Sonoma', 'Solano', 'Monterey', 'Marin', 'Merced', 'San Luis Obispo', 'Yolo', 'Santa Cruz', 'Placer', 'Napa', 'Humboldt', 'Madera', 'El Dorado', 'San Benito', 'Del Norte', 'Sutter', 'Nevada', 'Butte', 'Shasta', 'Mono', 'Mendocino', 'Yuba', 'Lake', 'Inyo', 'Mariposa', 'Calaveras', 'Glenn', 'Amador', 'Siskiyou', 'Colusa', 'Lassen', 'Tehama', 'Plumas', 'Tuolumne', 'Alpine', 'Modoc', 'Sierra', 'Trinity', 'Alameda - Berk', 'Yuba-Sutter']
-#
 # for county in counties:
 #     print(county, get_county_data(county))
 
-
+counties = ['Los Angeles', 'Riverside', 'San Diego', 'Orange', 'San Bernardi', 'Alameda', 'Santa Clara', 'San Francisco', 'San Mateo', 'Kern', 'Tulare', 'Santa Barbara', 'Fresno', 'Imperial', 'Contra Costa', 'Sacramento', 'Ventura', 'San Joaquin', 'Kings', 'Stanislaus', 'Sonoma', 'Solano', 'Monterey', 'Marin', 'Merced', 'San Luis Obispo', 'Yolo', 'Santa Cruz', 'Placer', 'Napa', 'Humboldt', 'Madera', 'El Dorado', 'San Benito', 'Del Norte', 'Sutter', 'Nevada', 'Butte', 'Shasta', 'Mono', 'Mendocino', 'Yuba', 'Lake', 'Inyo', 'Mariposa', 'Calaveras', 'Glenn', 'Amador', 'Siskiyou', 'Colusa', 'Lassen', 'Tehama', 'Plumas', 'Tuolumne', 'Alpine', 'Modoc', 'Sierra', 'Trinity', 'Alameda - Berk', 'Yuba-Sutter']
+for county in counties:
+    print(county)
+    print(county, get_county_climate(county, '5'))
+    print('\n')
 # https://stackoverflow.com/questions/36129963/use-beautifulsoup-to-obtain-view-element-code-instead-of-view-source-code
 
 # </span><div class="mat-ripple mat-button-ripple" matripple=""></div><div class="mat-button-focus-overlay"></div></button><!-- --><mat-menu class=""><!-- --></mat-menu></menu-item-more></nav></lib-menu><!-- --><div _ngcontent-app-root-c137=""></div><lib-search _ngcontent-app-root-c137="" _nghost-app-root-c135=""
--- test.py
+++ test.py
@@ -1,4 +1,19 @@
-list = [' a']
+import datetime
+from datetime import datetime, timedelta
 
-for i in list:
-    print(i.strip())
+# date_aot = str(datetime.today())[0:10]
+#
+# days = date_aot[-2:]
+# day = int(date_aot[-2:])
+#     day = 9
+#     if day <= 9:
+#         day = day[-1]
+# print(day)
+import os
+
+num = '09'
+county = "los-angeles"
+dir_path = os.path.dirname(os.path.realpath(__file__))
+dir_path = dir_path + '/csv/' + county + '.csv'
+
+print(dir_path)
